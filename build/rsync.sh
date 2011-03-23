c2a6d84b65705ad221de6622dca5c817cc2a98cb
Stephane Lacoin <slacoin@nuxeo.com>
2011-03-23 01:51:09 +0100

    fixing synched directory path

--- build/rsync.sh
+++ build/rsync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -av --delete --progress repository/target/. osgi@osgi.nuxeo.org:~/www/p2/ecr/current/.
+dir=$(dirname $0)
+rsync -av --delete --progress $dir/repository/target/. osgi@osgi.nuxeo.org:~/www/p2/ecr/current/.
