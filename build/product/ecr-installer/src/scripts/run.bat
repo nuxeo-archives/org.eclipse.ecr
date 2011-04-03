388a9ce55eae66e1431cd0c4e567d900dbec360c
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2011-04-03 13:03:54 +0200

    added ecr script launcher for windows (tested in wine)

---
+++ build/product/ecr-installer/src/scripts/run.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM the Java binary
+set JAVA=java
+
+REM the class path
+FOR %%c in (plugins\org.eclipse.equinox.launcher_*.jar) DO set JAVA_CP=plugins\%%c
+
+REM current directory
+set WD=%~dp0
+
+set JAVA_OPTS=-Dnuxeo.home=%WD%ecr -Declipse.ignoreApp=true -Dosgi.noShutdown=true
+
+REM uncomment this to enable debug
+REM set JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n
+
+REM The launcher arguments
+set DIRS=-configuration file:configuration/ -install file:./
+set ARGS=-arch x86_64 -nl en_US -consoleLog
+
+
+%JAVA% %JAVA_OPTS% -classpath %JAVA_CP% org.eclipse.equinox.launcher.Main %DIRS% %ARGS% %*
