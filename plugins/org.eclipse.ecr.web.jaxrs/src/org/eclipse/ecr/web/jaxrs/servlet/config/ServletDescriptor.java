58b6df2ae1b49357954aca3ad848171e1973d0a9
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2011-04-15 16:58:23 +0200

    ECR-10 - added support for servlet context getResource

--- plugins/org.eclipse.ecr.web.jaxrs/src/org/eclipse/ecr/web/jaxrs/servlet/config/ServletDescriptor.java
+++ plugins/org.eclipse.ecr.web.jaxrs/src/org/eclipse/ecr/web/jaxrs/servlet/config/ServletDescriptor.java
@@ -45,6 +45,9 @@ public class ServletDescriptor {
     @XNode("@path")
     protected String path;
 
+    @XNode("resources")
+    protected String resources;
+
     /**
      * Must use hashtable since it extends Dictionary
      */
@@ -87,6 +90,10 @@ public class ServletDescriptor {
 
     public ListenerSetDescriptor getListenerSet() {
         return listeners;
+    }
+
+    public String getResources() {
+        return resources;
     }
 
     public FilterSet[] getFilters() throws Exception {
