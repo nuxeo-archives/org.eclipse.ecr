b339a4a79d6bbfa0fe0009846e8d1316c9231ca5
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2011-07-20 17:11:16 +0200

    cleanup - as required by eclipse contribution

--- plugins/org.eclipse.ecr.convert/src/org/eclipse/ecr/convert/impl/FullTextConverter.java
+++ plugins/org.eclipse.ecr.convert/src/org/eclipse/ecr/convert/impl/FullTextConverter.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2002-2007 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * Copyright (c) 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
